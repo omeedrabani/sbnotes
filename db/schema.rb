190bb253a32cc75816bea5f573a3d29049ae8bff
Gabriel Von Meier <vonmeier@umail.ucsb.edu>
2015-04-25 18:42:06 -0700

    added user class and some tests for validity such as name and email not being blank, restrictions on length, no duplicate emails, made all db entries lowercase so its not case sensitive. next step is adding a hashed password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418205008) do
+ActiveRecord::Schema.define(version: 20150426013619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,5 +23,14 @@ ActiveRecord::Schema.define(version: 20150418205008) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
