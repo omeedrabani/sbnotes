1c01650e3fc5f77da59ae0f658f2415f897cd5a1
Andrew Tran <andrew.tran345@gmail.com>
2015-04-27 19:32:45 -0700

    WIP: Modifiation of stylesheet for Sign Up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      flash[:success] = "Congratulations on your new SBNotes account!"
   		redirect_to "/notes"
   	else
   		render 'new'
