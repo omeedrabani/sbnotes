fac5fa4309295d1c9894268f230513a65c24bce8
nickkpoon <nickkpoon@gmail.com>
2015-05-04 21:03:10 -0700

    remember me live

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       remember user
       redirect_to user
     else
@@ -19,8 +20,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-  	log_out
-  	redirect_to '/'
+  	log_out if logged_in?
+  	redirect_to root_url
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :notes, dependent: :destroy
+	attr_accessor :remember_token #remember me
 	before_save {self.email = email.downcase}
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -28,14 +29,19 @@ class User < ActiveRecord::Base
 
 
  #remembers the user in the database (remember ActiveModel)
- def remember
+  def remember
     self.remember_token = User.new_token																																		
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
 # check if remember token matches with user's remember digest
   def authenticated?(remember_token)
+  	return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
 
