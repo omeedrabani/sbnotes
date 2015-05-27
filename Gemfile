43b45f4008fc3e6c600e9e94612858a967ceed55
Gabriel Von Meier <vonmeier@umail.ucsb.edu>
2015-05-26 20:34:27 -0700

    -revised models/note.rb code to run search without using elasticsearch
    -added an extra parameter in the same self.search code to search through note text also, not just titles
    
    -uninstalled all gems related to elasticsearch and bonsai and removed them from the gemfile

--- Gemfile
+++ Gemfile
@@ -29,11 +29,6 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
-#search
-gem 'elasticsearch-model'
-gem 'elasticsearch-rails'
-gem 'bonsai-elasticsearch-rails'
-
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,19 +1,15 @@
-require 'elasticsearch/model'
-
 class Note < ActiveRecord::Base
-	include Elasticsearch::Model
-	include Elasticsearch::Model::Callbacks
 
 	belongs_to :user
 	validates :title, presence: true
 
 	def self.search(search)
+	  query = "%#{search}%"
 	  if search
-	    self.where("title like ?", "%#{search}%")
+	    self.where("title like ? or text like ?", query, query)
 	  else
 	    self.all
 	  end
 	end
 end
 
-Note.import #for autosync thing with elasticsearch
