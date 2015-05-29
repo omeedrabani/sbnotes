e780e52593f5cfec1b74cbe612642bc3745db4f2
Omeed Rabani <omeedrabani@omeed-rabanis-macbook-pro.local>
2015-05-28 18:01:10 -0700

    Removed some more plugins that would be confusing for the user.
    - Plugins such as language, html options, spellcheck (confusing, didn't work), etc. were also removed so as to prevent confusing users and to clean up any unnecessary features.
    - Also, I changed "Notes" to "My Notes" to different even further public notes and a user's personal notes.
    
    TO DO:
    - There seems to be a bug where if you make a table in a note, it will not display anything in your note. You have to remove the table in order for the rest of the things to show. We'll need to figure out how to fix this or just decide to get rid of the option to make a table in a note.

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -2,5 +2,5 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	config.removePlugins = 'image,flash,flashdialog';
+	config.removePlugins = 'image,flash,flashdialog,forms,about,pastefromword,newpage,div,language,scayt,wsc';
 };
