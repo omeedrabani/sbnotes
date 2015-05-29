393171941bf1e52a2dc018d7b0e2eacb0ea3c9b6
Omeed Rabani <omeedrabani@omeed-rabanis-macbook-pro.local>
2015-05-28 23:54:37 -0700

    Changed some files in order to have CKEditor work when deployed.

---
+++ config/initializers/ckeditor.rb
@@ -0,0 +1,10 @@
+Ckeditor.setup do |config|
+  config.assets_languages = ['en']
+  config.assets_plugins = [ 'basicstyles', 'clipboard', 'toolbar', 'enterkey', 'entities', 'floatingspace', 'wysiwygarea',
+  							'indentlist', 'link', 'list', 'undo', 'a11yhelp', 'blockquote', 'contextmenu', 'resize', 'elementspath',
+  							'filebrowser', 'format', 'horiztonalrule', 'htmlwriter', 'magicline', 'pastetext', 
+  							'removeformat', 'showborders', 'sourcearea', 'specialchar', 'stylescombo', 'tab', 'table',
+  							'tabletools', 'dialogadvtab', 'bidi', 'colorbutton', 'colordialog', 'templates', 'find',
+  							'font', 'iframe', 'indentblock', 'smiley', 'justify', 'liststyle',
+  							'pagebreak', 'preview', 'print', 'save', 'selectall', 'showblocks']
+end
