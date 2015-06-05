f735a3c805a8022518874c61a1671ab57a3b7936
Andrew Tran <andrew.tran345@gmail.com>
2015-06-05 00:58:39 -0700

    Added more unit and integration tests.

--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@note = Note.new(title: "Example", text: "Lorem ipsum something!",
+  		is_public: true)
+  end
+
+  test "should be valid" do
+  	assert @note.valid?
+  end
+
+  test "title should be present" do
+  	@note.title = ""
+  	assert_not @note.valid?
+  end
+
+  test "empty text should be valid" do
+  	@note.text = ""
+  	assert @note.valid?
+  end
+
 end
